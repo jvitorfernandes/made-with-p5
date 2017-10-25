1a52b37e8535a47753f601ec4e92c126e1099dc2
João <fernandesjvb@gmail.com>
2017-10-25 19:08:04 -0200

    identify hits

--- flappy-bird/sketch.js
+++ flappy-bird/sketch.js
@@ -10,20 +10,24 @@ function setup() {
 
 function draw() {
 	background(0);
+
+	for(var i = pipes.length-1; i >= 0 ; i--){
+		pipes[i].show();
+		pipes[i].update();
+
+		if(pipes[i].hits(bird)){
+		}
+
+		if(pipes[i].offscreen()){
+			pipes.splice(i, 1);
+		}
+	}
+
 	bird.show();
 	bird.update();
 
 	if(frameCount % 80 == 0){
 		pipes.push(new Pipe());
-	}
-
-	for(var i = pipes.length; i >= 0 ; i--){
-		pipes[i].show();
-		pipes[i].update();
-
-		if(pipes[i].offscreen()){
-			pipes.splice(i, 1);
-		}
 	}
 
 }
